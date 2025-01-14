58cc132d085ab09419ba33ab2e3cbec3d92eba19
Shashwot90 <shashwotpradhan@gmail.com>
2025-01-14 06:40:53 +0545

    prompt

--- app/chains.py
+++ app/chains.py
@@ -18,5 +18,6 @@ class Chain:
                                 {page_data}
                                 ### INSTRUCTION:
                                 The scraped text is from the career's page of a website.
-Your job is to extract the job postings and return them in JSON format containing the following keys: `role`, `experience`, `skills` and `description`.
-Only return the valid JSON.
+                                Your job is to extract the job postings and return them in JSON format containing the following keys: `role`, `experience`, `skills` and `description`.
+                                Only return the valid JSON.
+### VALID JSON (NO PREAMBLE):
