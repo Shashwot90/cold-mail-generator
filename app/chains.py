172a4e9907e13f90c28297739ab2b9205c786cd4
Shashwot90 <shashwotpradhan@gmail.com>
2025-01-17 06:22:28 +0545

    json parser

--- app/chains.py
+++ app/chains.py
@@ -27,3 +27,4 @@ class Chain:
             chain_extract = prompt_extract | self.llm
             res = chain_extract.invoke(input={"page_data": cleaned_text})
             try:
+                json_parser = JsonOutputParser()
