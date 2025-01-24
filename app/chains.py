0fc9ca3d78c508c112a18b1d6d890e57b75045ca
Shashwot90 <shashwotpradhan@gmail.com>
2025-01-24 06:51:46 +0545

    propmpt

--- app/chains.py
+++ app/chains.py
@@ -42,4 +42,5 @@ def write_mail(self, job, links):
 ### INSTRUCTION:
         You are Mohan, a business development executive at AtliQ. AtliQ is an AI & Software Consulting company dedicated to facilitating
         the seamless integration of business processes through automated tools. 
+        Over our experience, we have empowered numerous enterprises with tailored solutions, fostering scalability, 
         
