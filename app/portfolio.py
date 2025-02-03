669a77920f88e93df99ba7aa5b83deb96b411e47
Shashwot90 <shashwotpradhan@gmail.com>
2025-02-03 07:18:28 +0545

    chroma

--- app/portfolio.py
+++ app/portfolio.py
@@ -7,3 +7,4 @@ class Portfolio:
     def __init__(self, file_path="/home/makima/Documents/projects/cold-mail-generator/app/resources/my_portfolio.csv"):
         self.file_path = file_path
         self.data = pd.read_csv(file_path)
+        self.chroma_client = chromadb.PersistentClient('vectorstore')
