58afc1963e675072a35d05b3556778ce8888b3dc
Shashwot90 <shashwotpradhan@gmail.com>
2025-02-04 07:08:35 +0545

    collectuion

--- app/portfolio.py
+++ app/portfolio.py
@@ -8,3 +8,5 @@ class Portfolio:
         self.file_path = file_path
         self.data = pd.read_csv(file_path)
         self.chroma_client = chromadb.PersistentClient('vectorstore')
+        self.collection = self.chroma_client.get_or_create_collection(name="portfolio")
+
