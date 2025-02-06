ee464993f8787611309451d698b7c774743a51ce
Shashwot90 <shashwotpradhan@gmail.com>
2025-02-06 06:56:56 +0545

    text

--- app/utils.py
+++ app/utils.py
@@ -3,3 +3,6 @@ import re
 def clean_text(text):
     # Remove HTML tags
     text = re.sub(r'<[^>]*?>', '', text)
+    # Remove URLs
+    text = re.sub(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', '', text)
+    # Remove special characters
