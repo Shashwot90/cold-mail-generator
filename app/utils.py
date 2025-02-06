213efbf4a32262982f19676a90739712baf34074
Shashwot90 <shashwotpradhan@gmail.com>
2025-02-06 06:57:07 +0545

    text

--- app/utils.py
+++ app/utils.py
@@ -6,3 +6,5 @@ def clean_text(text):
     # Remove URLs
     text = re.sub(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', '', text)
     # Remove special characters
+    text = re.sub(r'[^a-zA-Z0-9 ]', '', text)
+    # Replace multiple spaces with a single space
